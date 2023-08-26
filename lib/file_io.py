db6131d9cab3b9535262415c501385e391fcad06
Kamente <justinm48000@gmail.com>
2023-08-26 12:45:19 +0200

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,22 +1,13 @@
 def write_file(file_name, file_content):
-    with open('file_name.txt', mode='w') as file:
+    with open(f'{file_name}.txt', mode='w') as file:
         file.write(file_content)
-        
-    # with open(file_name, 'w') as file:
-    #     file.write(file_content)
-
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode = 'a') as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    with open(f'{file_name}.txt' , mode = 'r') as file:
+    with open (f'{file_name}.txt', mode = 'r') as file:
         content = file.read()
     return content
-
-
-# text_file = open("file_nam", "mode")
-# contents = read(text_file)
-# print(contents)
-
-
